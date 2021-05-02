02e1ba0bea66d7ecdb17816ef1d42dce824f21b7
midhun-v <narenmids@gmail.com>
2021-05-02 11:53:08 +0530

    Questions added

---
+++ Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Midhun V on 01/05/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    var q: String
+    var a: String
+}
--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let questions = [
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    
+    var questionNumber = 0
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionFunc()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        if sender.currentTitle! == questions[questionNumber].a {
+            print("Correct answer")
+            score += 1
+            sender.backgroundColor = UIColor.green
+        }
+        else{
+            print("wrong answer")
+            sender.backgroundColor = UIColor.red
+        }
+        
+        print(questions.count-1 , questionNumber)
+        
+        if ((questions.count-1) > questionNumber) {
+            questionNumber += 1
+            
+            //Delays to execute the function in the time
+            Timer.scheduledTimer(timeInterval: 0.4, target: self, selector: #selector(questionFunc), userInfo: nil, repeats: false)
+        }
+        else if ((questions.count-1) == questionNumber) {
+            print ("Your score is", score)
+            viewDidLoad()
+        }
+        
+        print(questions.count-1 , questionNumber)
+    }
+    
+    @objc func questionFunc() {
+        questionLabel.text = questions[questionNumber].q
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+   
 }
 
